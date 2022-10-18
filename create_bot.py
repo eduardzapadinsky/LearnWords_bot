c7c45adbc321f4cf57246c7447964c3b80c39854
Zapadynsky <e.zapadinsky@gmail.com>
2022-10-18 22:35:21 +0300

    туче

--- create_bot.py
+++ create_bot.py
@@ -3,6 +3,6 @@ from aiogram import Bot, Dispatcher
 import config
 
 
-# bot = Bot(token=config.TOKEN)
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=config.TOKEN)
+# bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
